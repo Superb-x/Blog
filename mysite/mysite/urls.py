102a3df2b86b74ce7efef42b66bfa9ec6b0789cb
liuxianglin <nearwind_x@163.com>
2017-05-08 21:37:00 +0800

    add the index.html to the templates dir

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from server.tpl import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='blog/index.html')),
     url(r'^todo/', Todo.as_view()),
     url(r'^api/', include('server.urls', namespace='api')),
 ]
