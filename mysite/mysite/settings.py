2d740b8cc3a5261f283560a3099030d0adae2b66
liuxianglin <nearwind_x@163.com>
2017-07-01 23:42:35 +0800

    remove the corsheader middleware

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
 ]
 # 允许跨域
 CORS_ORIGIN_ALLOW_ALL = True
