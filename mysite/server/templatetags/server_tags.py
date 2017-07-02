8e5eda6932dc4f1e9a981264df2e7a7d9b26de77
liuxianglin <nearwind_x@163.com>
2017-07-02 09:32:41 +0800

    add the category count

--- mysite/server/templatetags/server_tags.py
+++ mysite/server/templatetags/server_tags.py
@@ -1,6 +1,7 @@
 import datetime
 from django import template
 from ..models import Post,Category
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -18,4 +19,4 @@ def archives():
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
