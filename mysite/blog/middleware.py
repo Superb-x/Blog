dc64c1ae62ea36ad2a07fbcbe3d2498f706299db
Superb-x <943572695@qq.com>
2017-07-14 11:23:55 +0800

    save the ip

--- mysite/blog/middleware.py
+++ mysite/blog/middleware.py
@@ -32,7 +32,7 @@ class VisitRecordMiddleware(MiddlewareMixin):
                 remote_ip = request.META['REMOTE_ADDR']
 
             print(remote_ip)
-            
+
             visitor_record.http_host = request.META.get('HTTP_HOST')
             visitor_record.http_user_agent = request.META.get('HTTP_USER_AGENT')
             visitor_record.ip = str(remote_ip)
