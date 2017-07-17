facd956b1e4a124aa95c24664e9ceed515e12d2e
Superb-x <943572695@qq.com>
2017-07-17 18:06:56 +0800

    process the images uploader

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 import xadmin
 from django.conf.urls import url, include
-from django.contrib import admin
-from django.views.generic.base import TemplateView
 from blog.tpl import *
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 xadmin.autodiscover()
 
@@ -29,5 +28,5 @@ urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
