6a9d587fb4865a44da5be2d513affb6a05cc6c91
Superb-x <943572695@qq.com>
2017-09-01 14:48:28 +0800

    comfort the mobile and add the site search

--- mysite/blog/static/js/index.js
+++ mysite/blog/static/js/index.js
@@ -3,8 +3,20 @@
  */
 $(function () {
     // 缓存DOM 节点,JQ优化技巧之一
-    var $item = $(".item-spread");
+    var $item = $(".item-spread"), // 展开项
+        $mask = $("#mask"),        // 遮罩层
+        $wrap = $(".wrapper");     // 顶层容器
     $item.click(function () {
         $(this).toggleClass("active").siblings().removeClass("active");
+    });
+    $(".menu-btn").click(function () {
+        $(".wrapper").toggleClass("opened");
+    });
+    $mask.on("touchmove", function (e) {
+        e.preventDefault();
+        return false
+    });
+    $mask.click(function () {
+         $wrap.removeClass("opened");
     })
-})
+});
